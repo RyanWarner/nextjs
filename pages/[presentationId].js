11abcbbf9a515b072b94993d600417f5c02eae99
Ryan Warner <warner.ryan@gmail.com>
2020-06-21 19:49:49 -0400

    Add og:image

--- pages/[presentationId].js
+++ pages/[presentationId].js
@@ -7,8 +7,9 @@ const Post = (props) => {
   const router = useRouter()
   const { presentationId } = router.query
 
-  const book = Object.keys(props.books)[0]
-  const title = props.books[book].title
+  const key = Object.keys(props.books)[0]
+  const book = props.books[key]
+  const title = book.title
 
   console.log('title', title)
 
@@ -17,6 +18,10 @@ const Post = (props) => {
       <Head>
         <title>{title}</title>
         <meta property='og:title' content={title} />
+        <meta property='og:image' content={book.cover.medium} />
+        <meta name='twitter:site' content='@RyanWarnerCodes' />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:image' content={book.cover.medium} />
       </Head>
       <p>Presentation: {presentationId}</p>
       <p>Books SSR: {title}</p>
